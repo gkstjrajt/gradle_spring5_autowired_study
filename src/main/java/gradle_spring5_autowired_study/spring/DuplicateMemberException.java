dfc16fdf40dd3c663c5076a5a515561086930806
hanseok <gkstjrajt@gmail.com>
2020-09-23 09:36:00 +0900

    스프링5 프로그래밍 입문 chap04 초기화 완료

---
+++ src/main/java/gradle_spring5_autowired_study/spring/DuplicateMemberException.java
@@ -0,0 +1,9 @@
+package gradle_spring5_autowired_study.spring;
+
+public class DuplicateMemberException extends RuntimeException {
+
+	public DuplicateMemberException(String message) {
+		super(message);
+	}
+
+}
---
+++ src/main/java/gradle_spring5_autowired_study/spring/MemberNotFoundException.java
@@ -0,0 +1,5 @@
+package gradle_spring5_autowired_study.spring;
+
+public class MemberNotFoundException extends RuntimeException {
+
+}
